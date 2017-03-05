e7721131b01a7f0a63da75a29ff9415d36790eee
Alexander Pliskin <alexnetmore@gmail.com>
2017-03-05 20:40:33 +0300

    Add comments

--- task4.m
+++ task4.m
@@ -1,13 +1,26 @@
 n = input('');
+% get_r() - function to create R
 rng(get_r());
 arr = randi(10, n, 1);
+
+% print_info - function to display author info
 print_info();
 fprintf('%d\n', n);
 fprintf('%d ', arr)
 res = 0;
+% Variant 1 : definition of couple in array - two adjacent elements
 for i=(1:n-1)
     if (arr(i) + arr(i+1) > 0 && mod(arr(i)+arr(i+1),2)~=0)
         res = res + 1;
      end;
 end;
-fprintf('\n%d\n', res);
+fprintf('\n[Var1] : %d\n', res);
+% Variant 2 : couple - two any elemnts of an array with different index
+for i=(1:n-1)
+    for j=(i+1:n-1)
+        if (arr(i) + arr(j) > 0 && mod(arr(i)+arr(j),2)~=0)
+            res = res + 1;
+        end;
+     end;
+end;
+fprintf('[Var2] : %d\n', res);
--- task5.m
+++ task5.m
@@ -1,8 +1,10 @@
 n = input('');
 m = input('');
+% get_r() - function to create R
 rng(get_r());
 arr = randi(10, n, m);
 
+% print_info - function to display author info
 print_info();
 fprintf('%d, %d\n', n, m);
 
