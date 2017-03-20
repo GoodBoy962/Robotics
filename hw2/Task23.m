057caaccaab44e86e559d46fe591758b8b2d2a41
Alexander Pliskin <alexnetmore@gmail.com>
2017-03-20 13:29:06 +0300

    updates

--- hw2/Task23.m
+++ hw2/Task23.m
@@ -13,8 +13,8 @@ function [] = Task23( T )
     
     P1 = (T * P.')';
     
-    plot3(P(:,1),P(:,2),P(:,3))
+    plot3(P(:,1),P(:,2),P(:,3));
     hold on;
-    plot3(P1(:,1),P1(:,2),P1(:,3))
+    plot3(P1(:,1),P1(:,2),P1(:,3));
     tranimate(T);
 end
