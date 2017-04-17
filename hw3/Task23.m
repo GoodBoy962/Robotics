5650450ad7d42110663e2448c4095abf3dcd1c80
Alexander Pliskin <alexnetmore@gmail.com>
2017-04-17 11:08:36 +0300

    changes

--- hw3/Task23.m
+++ hw3/Task23.m
@@ -1,7 +1,9 @@
+clear;
+
 l(1) = Link([0, 1, 0, 0, 1]);
 l(2) = Link([0, 1, 0, -pi/2, 1]);
 l(3) = Link([0, 0.5, 0, -pi/2, 0]);
 l(4) = Link([0,0,0,0,0]); % end effector
 
-R = SerialLink(l, 'name', 'Figure2');
-R.plot([0, 1, 2, 3], 'workspace', [-2 2 -2 2 -2 2]);
+Robot = SerialLink(l, 'name', 'Figure 2');
+Robot.plot([0, 1, 2, 3], 'workspace', [-2 2 -2 2 -2 2]);
