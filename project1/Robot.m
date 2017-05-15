3c860ac1cdeda103bf435c0c1bfbe99e3a7ecbcb
Alexander Pliskin <alexnetmore@gmail.com>
2017-05-16 01:06:09 +0300

    WHERE function implemetation added

--- project1/Robot.m
+++ project1/Robot.m
@@ -11,6 +11,12 @@ R = SerialLink(l, 'name', 'Robot');
 %DH table
 disp(R.char);
 
-R.plot([0,1,2,3,4,5,6], 'workspace', [-5,5,-5,5,-5,5]);
-% R.teach()
+%Get transformation matrix || Where function is the last column with 3
+%first rows || It's result is an answer
+R.fkine([pi/2, pi/2, pi/2, pi/2, pi/2, pi/2, 0])
+
+l(3).qlim = [0, 2];
+l(6).qlim = [0, 2];
+R.plot([0,1,2,3,4,5,6], 'workspace', [-10,10,-10,10,-10,10]);
+R.teach()
 
