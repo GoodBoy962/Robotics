49a347acf5f758dfdd9cf4c8282b40609a042090
vlad-matyunin96 <vlad.matyunin96@gmail.com>
2017-05-18 18:29:57 +0300

    reformatted WHERE function

--- project1/Robot.m
+++ project1/Robot.m
@@ -1,10 +1,11 @@
-l(1) = Link([0,1,0,-pi/2,0]);
-l(2) = Link([0,1,0,pi/2,0]);
-l(3) = Link([-pi/2, 1, 0, 0, 1]);
-l(4) = Link([0,1,0,-pi/2,0]);
-l(5) = Link([0,0,0,pi/2,0]);
-l(6) = Link([0,1,0,0,1]);
-l(7) = Link([0,0,0,0,0]);
+l(1) = Link([-pi/2, 1, 0, 0, 1]);
+l(2) = Link([0,1,0,-pi/2,0]);
+l(3) = Link([0,1,0,pi/2,0]);
+l(4) = Link([-pi/2, 1, 0, 0, 1]);
+l(5) = Link([0,1,0,-pi/2,0]);
+l(6) = Link([0,0,0,pi/2,0]);
+l(7) = Link([0,1,0,0,1]);
+l(8) = Link([0,0,0,0,0]);
 
 R = SerialLink(l, 'name', 'Robot');
 
@@ -13,10 +14,30 @@ disp(R.char);
 
 %Get transformation matrix || Where function is the last column with 3
 %first rows || It's result is an answer
-R.fkine([pi/2, pi/2, pi/2, pi/2, pi/2, pi/2, 0])
+%R.fkine([pi/2, pi/2, 1, pi/2, pi/2, pi/2, 0]);
+where(R,[0,pi/2, pi/2, 1, pi/2, pi/2, pi/2, 0],[5,5,5,5,5,5,5,5])
+%l(3).qlim = [0, 2];
+%l(6).qlim = [0, 2];
+%R.plot([0,1,2,3,4,5,6], 'workspace', [-10,10,-10,10,-10,10]);
+%R.teach()
+%prJoint1 = Prismatic();
+%revJoint1 = Revolute();
+%robot = SerialLink([prJoint1 revJoint1],'name','ROBOT_THE_BEST');
 
-l(3).qlim = [0, 2];
-l(6).qlim = [0, 2];
-R.plot([0,1,2,3,4,5,6], 'workspace', [-10,10,-10,10,-10,10]);
-R.teach()
-
+function [point,rotation_matrix]=where(Robot,angles, extensions)
+links = Robot.links;
+thetas=[];
+for i = 1:length(links)
+   if(~links(i).isprismatic)
+       thetas=[thetas,angles(i)];
+   else
+       thetas=[thetas,0];
+       links(i).qlim=[0,extensions(i)];
+   end
+end
+transformMatrix=Robot.fkine(thetas);
+point=transformMatrix(1:3,4);
+rotation_matrix=transformMatrix(1:3,1:3);
+Robot.plot([0,1,2,3,4,5,6,7], 'workspace', [-10,10,-10,10,-10,10]);
+Robot.teach();
+end
