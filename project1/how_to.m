ddd4d999e6e74c5673aaa1f41cc9781689743a51
Alexander Pliskin <alexnetmore@gmail.com>
2017-05-20 19:21:55 +0300

    ws_display initialized

---
+++ project1/how_to.m
@@ -0,0 +1,6 @@
+function [ output_args ] = how_to( input_args )
+
+
+
+end
+
--- project1/ws_display.m
+++ project1/ws_display.m
@@ -1,7 +1,25 @@
-function [ output_args ] = ws_display( input_args )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function [ ] = ws_display()
 
+R = get_robot();
+
+q0=[0 0 0 0 0 0 0 0];
+q1b=[3*pi .1745 pi/3 pi/2 .5236 pi/1.5 0 0];
+q1=[.0873 .1745 .3491 .1745 .5236 .8727 0];
+q2=[.0873 0 -.3491 .2618 .8727 1.2217 0];
+q3=[-.1745 -.3491 .5236 .0873 -.1222 .3491 0];
+q4=[.1745 .1745 0 -.1396 .3191 -.5236 0];
+q5=[-.3491 -.3491 -.3491 0 -.0873 0 0];
+
+
+t=0:.03:2;%Time vector & steps
+traj1=jtraj(q0,q1,t); 
+traj2=jtraj(q1,q2,t);
+traj3=jtraj(q2,q3,t);
+traj4=jtraj(q3,q4,t);
+traj5=jtraj(q4,q5,t);
+traj6=jtraj(q5,q0,t);
+
+R.plot(traj1)
 
 end
 
