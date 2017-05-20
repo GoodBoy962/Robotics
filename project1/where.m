c4f7868573c7c3a59f807602fbb3557437d8531e
Alexander Pliskin <alexnetmore@gmail.com>
2017-05-21 02:04:52 +0300

    updates

--- project1/where.m
+++ project1/where.m
@@ -13,8 +13,9 @@ for i = 1:length(links)
    end
 end
 
+Robot.offset = extensions;
 transform_matrix = Robot.fkine(thetas);
-Robot.A([1,2,3,4,5,6,7,8], extensions);
+%Robot.A([1,2,3,4,5,6,7,8], extensions);
 
 Robot.plot([0,1,2,3,4,5,6,7], 'workspace', [-10,10,-10,10,-10,10]);
 
