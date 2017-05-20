d73ded47b8e8cb7b69824661139db7c8e71bd77f
Alexander Pliskin <alexnetmore@gmail.com>
2017-05-20 17:50:23 +0300

    updates

--- project1/Robot2.m
+++
@@ -1,6 +0,0 @@
-Robot = getRobot();
-
-where([0,pi/2, pi/2, 1, pi/2, pi/2, pi/2, 0],[5,5,5,5,5,5,5,5])
-%prJoint1 = Prismatic();
-%revJoint1 = Revolute();
-%robot = SerialLink([prJoint1 revJoint1],'name','ROBOT_THE_BEST');
