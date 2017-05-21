1fb3b9cdec741132f93de4d165564ddad6b8f98a
Alexander Pliskin <alexnetmore@gmail.com>
2017-05-21 21:48:46 +0300

    updates

--- project1/where_no_disp.m
+++ project1/where_no_disp.m
@@ -2,19 +2,8 @@ function [transform_matrix]=where_no_disp(angles, extensions)
 
 Robot = get_robot();
 
-links = Robot.links;
-thetas = [];
-
-for i = 1:length(links)
-   if(~links(i).isprismatic)
-       thetas(i) = angles(i);
-   else
-       thetas(i) = 0;
-   end
-end
-
 Robot.offset = extensions;
-transform_matrix = Robot.fkine(thetas);
+transform_matrix = Robot.fkine(angles);
 
 
 end
--- project1/ws_display.m
+++ project1/ws_display.m
@@ -1,71 +1,38 @@
 function [ ] = ws_display( )
 
-% R = get_robot();
-% 
-% % length of joints
-% l1 = extensions(1);
-% l4 = extensions(2);
-% l7 = extensions(7);
-% 
-% l2 = R.links(2);
-% l3 = R.links(3);
-% l5 = R.links(5);
-% l6 = R.links(6);
-% l8 = R.links(8);
-% 
-% % thetas possible values
-% theta1 = 0;
-% theta4 = 0;
-% theta7 = 0;
-% 
-% theta2 = -pi:0.1:pi;
-% theta3 = -pi:0.1:pi;
-% theta5 = -pi:0.1:pi;
-% theta6 = -pi:0.1:pi;
-% theta8 = -pi:0.1:pi;
-% 
-% [THETA1,THETA2,THETA3,THETA4,THETA5,THETA6,THETA7,THETA8] 
-%             = meshgrid(theta1,theta2,theta3,theta4,theta5,theta6,theta7,theta8);
-% 
-% q0=[0 0 0 0 0 0 0 0];
-% q1b=[3*pi .1745 pi/3 pi/2 .5236 pi/1.5 0 0];
-% q1=[.0873 .1745 .3491 .1745 .5236 .8727 0 0];
-% q2=[.0873 0 -.3491 .2618 .8727 1.2217 0];
-% q3=[-.1745 -.3491 .5236 .0873 -.1222 .3491 0];
-% q4=[.1745 .1745 0 -.1396 .3191 -.5236 0];
-% q5=[-.3491 -.3491 -.3491 0 -.0873 0 0];
-% 
-% 
-% t=0:.03:2;%Time vector & steps
-% traj1=jtraj(q0,q1,t); 
-% % traj2=jtraj(q1,q2,t);
-% % traj3=jtraj(q2,q3,t);
-% % traj4=jtraj(q3,q4,t);
-% % traj5=jtraj(q4,q5,t);
-% % traj6=jtraj(q5,q0,t);
-% 
-% % R.links(1).qlim = [0, 2];
-% % R.links(4).qlim = [0, 2];
-% % R.links(7).qlim = [0, 2];
-% 
-% R.plot(traj1, 'workspace', [-10,10,-10,10,-10,10])
+ANGLE_STEP = pi/2;
+EXTEN_STEP = 1;
 
 x = [];
 y = [];
 z = [];
+
 Robot = get_robot();
-for theta5 = 0 : pi/6 : 2*pi
-    for theta6 = 0 : pi/6 : 2*pi
-        t = Robot.fkine([0 0 -pi/2 0 theta5 theta6 0 0]);
-        pos = t(:,4)
-        x(end + 1) = pos(1);
-        y(end + 1) = pos(2);
-        z(end + 1) = pos(3);
+for l1 = 0 : EXTEN_STEP : 2
+    for theta2 = 0 : ANGLE_STEP : 2*pi
+        for theta3 = 0 : ANGLE_STEP : 2*pi
+            for l4 = 0 : EXTEN_STEP : 2
+                for theta5 = 0 : ANGLE_STEP : 2*pi
+                    for theta6 = 0 : ANGLE_STEP : 2*pi
+                        for l7 = 0 : EXTEN_STEP : 2
+                            for theta8 = 0 : ANGLE_STEP : 2*pi
+                                Robot.offset = [l1, 0, 0, l4, 0, 0, l7, 0];
+                                t = Robot.fkine([0, theta2, theta3, 0, theta5, theta6, 0, theta8]); 
+                                pos = t(:,4);
+                                x(end + 1) = pos(1);
+                                y(end + 1) = pos(2);
+                                z(end + 1) = pos(3);
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
 end
 
+Robot.plot([0, 0, 0, 0, 0, 0, 0, 0], 'workspace', [-15, 15, -15, 15, -15, 15]);
 scatter3(x, y, z);
-plot_robot(Robot);
 
 end
 
